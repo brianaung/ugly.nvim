e42bccaf0821efe0e89b7ab85062b5be1716185b
Brian Aung <brianaung16@gmail.com>
2023-12-11 00:05:08 +1100

    remove unused funcs

--- lua/ugly/util.lua
+++ lua/ugly/util.lua
@@ -21,20 +21,8 @@ M.syntax = function(syntax)
 end
 
 M.highlight = function(group, hl)
-	-- group = ts.get(group)
 	if not group then
 		return
-	end
-	if hl.style then
-		if type(hl.style) == "table" then
-			hl = vim.tbl_extend("force", hl, hl.style)
-		elseif hl.style:lower() ~= "none" then
-			-- handle old string style definitions
-			for s in string.gmatch(hl.style, "([^,]+)") do
-				hl[s] = true
-			end
-		end
-		hl.style = nil
 	end
 	vim.api.nvim_set_hl(0, group, hl)
 end
