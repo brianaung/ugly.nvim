8f2b2dd00e4a14b9e762173ce9a4f17369f5c277
Brian Aung <brianaung16@gmail.com>
2023-12-11 00:19:52 +1100

    fix not loading default options

--- lua/ugly/config.lua
+++ lua/ugly/config.lua
@@ -7,9 +7,14 @@ local defaults = {
 	-- ...
 }
 
--- setup with default options + user configs
+M.options = {}
+
+-- user can overwrite defaults with their config options.
 M.setup = function(options)
 	M.options = vim.tbl_deep_extend("force", {}, defaults, options or {})
 end
+
+-- initially, when we require this config, fillup M.options with defaults
+M.setup()
 
 return M
