a8ddade03165101fa6fe1d9d6191cdfdd26c762f
Brian Aung <brianaung16@gmail.com>
2023-12-11 03:12:54 +1100

    setup base colors hl groups

--- lua/ugly/theme.lua
+++ lua/ugly/theme.lua
@@ -1,5 +1,4 @@
--- local util = require("ugly.util")
-
+--[[ largely inspired by: https://github.com/neovim/neovim/pull/26334 ]]
 local M = {}
 
 M.setup = function()
@@ -8,72 +7,172 @@ M.setup = function()
 	local config = require("ugly.config")
 	local o = config.options
 	local c = require("ugly.colors").default
+	local bg = c.bg
+	local fg = c.fg
 
 	-- set highlight groups
 	theme.highlights = {
-		Comment = { fg = c.comment, style = o.styles.comments }, -- any comment
-		ColorColumn = { bg = c.black }, -- used for the columns set with 'colorcolumn'
-		Conceal = { fg = c.dark5 }, -- placeholder characters substituted for concealed text (see 'conceallevel')
-		Cursor = { fg = c.bg, bg = c.fg }, -- character under the cursor
-		lCursor = { fg = c.bg, bg = c.fg }, -- the character under the cursor when |language-mapping| is used (see 'guicursor')
-		CursorIM = { fg = c.bg, bg = c.fg }, -- like Cursor, but used when in IME mode |CursorIM|
-		CursorColumn = { bg = c.bg_highlight }, -- Screen-column at the cursor, when 'cursorcolumn' is set.
-		CursorLine = { bg = c.bg_highlight }, -- Screen-line at the cursor, when 'cursorline' is set.  Low-priority if foreground (ctermfg OR guifg) is not set.
-		Directory = { fg = c.blue }, -- directory names (and other special names in listings)
-		-- DiffAdd = { bg = c.diff.add }, -- diff mode: Added line |diff.txt|
-		-- DiffChange = { bg = c.diff.change }, -- diff mode: Changed line |diff.txt|
-		-- DiffDelete = { bg = c.diff.delete }, -- diff mode: Deleted line |diff.txt|
-		-- DiffText = { bg = c.diff.text }, -- diff mode: Changed text within a changed line |diff.txt|
-		EndOfBuffer = { fg = c.bg }, -- filler lines (~) after the end of the buffer.  By default, this is highlighted like |hl-NonText|.
-		-- TermCursor  = { }, -- cursor in a focused terminal
-		-- TermCursorNC= { }, -- cursor in an unfocused terminal
-		ErrorMsg = { fg = c.error }, -- error messages on the command line
-		VertSplit = { fg = c.border }, -- the column separating vertically split windows
-		WinSeparator = { fg = c.border, bold = true }, -- the column separating vertically split windows
-		Folded = { fg = c.blue, bg = c.fg_gutter }, -- line used for closed folds
-		FoldColumn = { bg = o.transparent and c.none or c.bg, fg = c.comment }, -- 'foldcolumn'
-		SignColumn = { bg = o.transparent and c.none or c.bg, fg = c.fg_gutter }, -- column where |signs| are displayed
-		SignColumnSB = { bg = c.bg_sidebar, fg = c.fg_gutter }, -- column where |signs| are displayed
-		Substitute = { bg = c.red, fg = c.black }, -- |:substitute| replacement text highlighting
-		LineNr = { fg = c.fg_gutter }, -- Line number for ":number" and ":#" commands, and when 'number' or 'relativenumber' option is set.
-		CursorLineNr = { fg = c.dark5 }, -- Like LineNr when 'cursorline' or 'relativenumber' is set for the cursor line.
-		MatchParen = { fg = c.orange, bold = true }, -- The character under the cursor or just before it, if it is a paired bracket, and its match. |pi_paren.txt|
-		ModeMsg = { fg = c.fg_dark, bold = true }, -- 'showmode' message (e.g., "-- INSERT -- ")
-		MsgArea = { fg = c.fg_dark }, -- Area for messages and cmdline
-		-- MsgSeparator= { }, -- Separator for scrolled messages, `msgsep` flag of 'display'
-		MoreMsg = { fg = c.blue }, -- |more-prompt|
-		NonText = { fg = c.dark3 }, -- '@' at the end of the window, characters from 'showbreak' and other characters that do not really exist in the text (e.g., ">" displayed when a double-wide character doesn't fit at the end of the line). See also |hl-EndOfBuffer|.
-		Normal = { fg = c.fg, bg = o.transparent and c.none or c.bg },
-		NormalNC = { fg = c.fg, bg = o.transparent and c.none or c.bg }, -- normal text in non-current windows
-		NormalSB = { fg = c.fg_sidebar, bg = c.bg_sidebar }, -- normal text in sidebar
-		NormalFloat = { fg = c.fg_float, bg = c.bg_float }, -- Normal text in floating windows.
-		FloatBorder = { fg = c.border_highlight, bg = c.bg_float },
-		FloatTitle = { fg = c.border_highlight, bg = c.bg_float },
-		Pmenu = { bg = c.bg_popup, fg = c.fg }, -- Popup menu: normal item.
-		PmenuSel = { bg = c.fg_gutter }, -- Popup menu: selected item.
-		PmenuSbar = { bg = c.bg_popup }, -- Popup menu: scrollbar.
-		PmenuThumb = { bg = c.fg_gutter }, -- Popup menu: Thumb of the scrollbar.
-		Question = { fg = c.blue }, -- |hit-enter| prompt and yes/no questions
-		QuickFixLine = { bg = c.bg_visual, bold = true }, -- Current |quickfix| item in the quickfix window. Combined with |hl-CursorLine| when the cursor is there.
-		Search = { bg = c.bg_search, fg = c.fg }, -- Last search pattern highlighting (see 'hlsearch').  Also used for similar items that need to stand out.
-		IncSearch = { bg = c.orange, fg = c.black }, -- 'incsearch' highlighting; also used for the text replaced with ":s///c"
-		CurSearch = { link = "IncSearch" },
-		SpecialKey = { fg = c.dark3 }, -- Unprintable characters: text displayed differently from what it really is.  But not 'listchars' whitespace. |hl-Whitespace|
-		SpellBad = { sp = c.error, undercurl = true }, -- Word that is not recognized by the spellchecker. |spell| Combined with the highlighting used otherwise.
-		SpellCap = { sp = c.warning, undercurl = true }, -- Word that should start with a capital. |spell| Combined with the highlighting used otherwise.
-		SpellLocal = { sp = c.info, undercurl = true }, -- Word that is recognized by the spellchecker as one that is used in another region. |spell| Combined with the highlighting used otherwise.
-		SpellRare = { sp = c.hint, undercurl = true }, -- Word that is recognized by the spellchecker as one that is hardly ever used.  |spell| Combined with the highlighting used otherwise.
-		StatusLine = { fg = c.fg_sidebar, bg = c.bg_statusline }, -- status line of current window
-		StatusLineNC = { fg = c.fg_gutter, bg = c.bg_statusline }, -- status lines of not-current windows Note: if this is equal to "StatusLine" Vim will use "^^^" in the status line of the current window.
-		TabLine = { bg = c.bg_statusline, fg = c.fg_gutter }, -- tab pages line, not active tab page label
-		TabLineFill = { bg = c.black }, -- tab pages line, where there are no labels
-		TabLineSel = { fg = c.black, bg = c.blue }, -- tab pages line, active tab page label
-		Title = { fg = c.blue, bold = true }, -- titles for output from ":set all", ":autocmd" etc.
-		Visual = { bg = c.bg_visual }, -- Visual mode selection
-		VisualNOS = { bg = c.bg_visual }, -- Visual mode selection when vim is "Not Owning the Selection".
-		WarningMsg = { fg = c.warning }, -- warning messages
-		Whitespace = { fg = c.fg_gutter }, -- "nbsp", "space", "tab" and "trail" in 'listchars'
-		WildMenu = { bg = c.bg_visual }, -- current match in 'wildmenu' completion
+		-- ui
+		ColorColumn = { bg = bg.grey4 },
+		Conceal = { fg = bg.grey4 },
+		CurSearch = { link = "Search" },
+		Cursor = {},
+		CursorColumn = { bg = bg.grey3 },
+		CursorIM = { link = "Cursor" },
+		CursorLine = { bg = bg.grey3 },
+		CursorLineFold = { link = "FoldColumn" },
+		CursorLineNr = { bold = true },
+		CursorLineSign = { link = "SignColumn" },
+		DiffAdd = { fg = fg.grey1, bg = bg.green },
+		DiffChange = { fg = fg.grey1, bg = bg.grey4 },
+		DiffDelete = { fg = fg.red, bold = true },
+		DiffText = { fg = fg.grey1, bg = bg.cyan },
+		Directory = { fg = fg.cyan },
+		EndOfBuffer = { link = "NonText" },
+		ErrorMsg = { fg = fg.red },
+		FloatBorder = { link = "NormalFloat" },
+		FloatShadow = { fg = bg.grey1, blend = 80 },
+		FloatShadowThrough = { fg = bg.grey1, blend = 100 },
+		FloatTitle = { link = "Title" },
+		FoldColumn = { link = "SignColumn" },
+		Folded = { fg = fg.grey4, bg = bg.grey3 },
+		IncSearch = { link = "Search" },
+		lCursor = { fg = bg.grey2, bg = fg.grey2 },
+		LineNr = { fg = bg.grey4 },
+		LineNrAbove = { link = "LineNr" },
+		LineNrBelow = { link = "LineNr" },
+		MatchParen = { bg = bg.grey4, bold = true },
+		ModeMsg = { fg = fg.green },
+		MoreMsg = { fg = fg.cyan },
+		MsgArea = {},
+		MsgSeparator = { link = "StatusLine" },
+		NonText = { fg = bg.grey4 },
+		Normal = { fg = fg.grey2, bg = o.transparent and c.none or bg.grey2 },
+		NormalFloat = { fg = fg.grey2, bg = bg.grey1 },
+		NormalNC = {},
+		PMenu = { fg = fg.grey2, bg = bg.grey3 },
+		PMenuExtra = { link = "PMenu" },
+		PMenuExtraSel = { link = "PMenuSel" },
+		PMenuKind = { link = "PMenu" },
+		PMenuKindSel = { link = "PMenuSel" },
+		PMenuSbar = { link = "PMenu" },
+		PMenuSel = { fg = bg.grey3, bg = fg.grey2, blend = 0 },
+		PMenuThumb = { bg = bg.grey4 },
+		Question = { fg = fg.cyan },
+		QuickFixLine = { bold = true },
+		RedrawDebugNormal = { reverse = true },
+		RedrawDebugClear = { bg = bg.cyan },
+		RedrawDebugComposed = { bg = bg.green },
+		RedrawDebugRecompose = { bg = bg.red },
+		Search = { fg = fg.grey1, bg = bg.yellow },
+		SignColumn = { fg = bg.grey4 },
+		SpecialKey = { fg = bg.grey4 },
+		SpellBad = { sp = fg.red, undercurl = true },
+		SpellCap = { sp = fg.yellow, undercurl = true },
+		SpellLocal = { sp = fg.green, undercurl = true },
+		SpellRare = { sp = fg.cyan, undercurl = true },
+		StatusLine = { fg = fg.grey3, bg = bg.grey1 },
+		StatusLineNC = { fg = fg.grey4, bg = bg.grey1 },
+		Substitute = { link = "Search" },
+		TabLine = { fg = fg.grey3, bg = bg.grey1 },
+		TabLineFill = { link = "Tabline" },
+		TabLineSel = { bold = true },
+		TermCursor = { reverse = true },
+		TermCursorNC = { reverse = true },
+		Title = { bold = true },
+		VertSplit = { link = "WinSeparator" },
+		Visual = { bg = bg.grey4 },
+		VisualNOS = { link = "Visual" },
+		WarningMsg = { fg = fg.yellow },
+		Whitespace = { link = "NonText" },
+		WildMenu = { link = "PMenuSel" },
+		WinBar = { link = "StatusLine" },
+		WinBarNC = { link = "StatusLineNC" },
+		WinSeparator = { link = "Normal" },
+
+		-- Syntax
+		Comment = { fg = fg.grey4 },
+
+		Constant = { fg = fg.grey2 },
+		String = { fg = fg.green },
+		Character = { link = "Constant" },
+		Number = { link = "Constant" },
+		Boolean = { link = "Constant" },
+		Float = { link = "Number" },
+
+		Identifier = { fg = fg.blue },
+		Function = { fg = fg.cyan },
+
+		Statement = { fg = fg.grey2, bold = true },
+		Conditional = { link = "Statement" },
+		Repeat = { link = "Statement" },
+		Label = { link = "Statement" },
+		Operator = { fg = fg.grey2 },
+		Keyword = { link = "Statement" },
+		Exception = { link = "Statement" },
+
+		PreProc = { fg = fg.grey2 },
+		Include = { link = "PreProc" },
+		Define = { link = "PreProc" },
+		Macro = { link = "PreProc" },
+		PreCondit = { link = "PreProc" },
+
+		Type = { fg = fg.grey2 },
+		StorageClass = { link = "Type" },
+		Structure = { link = "Type" },
+		Typedef = { link = "Type" },
+
+		Special = { fg = fg.grey2 },
+		Tag = { link = "Special" },
+		SpecialChar = { link = "Special" },
+		Delimiter = {},
+		SpecialComment = { link = "Special" },
+		Debug = { link = "Special" },
+
+		LspInlayHint = { link = "NonText" },
+		SnippetTabstop = { link = "Visual" },
+
+		Underlined = { underline = true },
+		Ignore = { link = "Normal" },
+		Error = { fg = bg.grey1, bg = fg.red },
+		Todo = { fg = fg.grey1, bold = true },
+
+		diffAdded = { fg = fg.green },
+		diffRemoved = { fg = fg.red },
+
+		-- built-in diagnostics
+		DiagnosticError = { fg = fg.red },
+		DiagnosticWarn = { fg = fg.yellow },
+		DiagnosticInfo = { fg = fg.cyan },
+		DiagnosticHint = { fg = fg.blue },
+		DiagnosticOk = { fg = fg.green },
+
+		DiagnosticUnderlineError = { sp = fg.red, underline = true },
+		DiagnosticUnderlineWarn = { sp = fg.yellow, underline = true },
+		DiagnosticUnderlineInfo = { sp = fg.cyan, underline = true },
+		DiagnosticUnderlineHint = { sp = fg.blue, underline = true },
+		DiagnosticUnderlineOk = { sp = fg.green, underline = true },
+
+		DiagnosticFloatingError = { fg = fg.red, bg = bg.grey1 },
+		DiagnosticFloatingWarn = { fg = fg.yellow, bg = bg.grey1 },
+		DiagnosticFloatingInfo = { fg = fg.cyan, bg = bg.grey1 },
+		DiagnosticFloatingHint = { fg = fg.blue, bg = bg.grey1 },
+		DiagnosticFloatingOk = { fg = fg.green, bg = bg.grey1 },
+
+		DiagnosticVirtualTextError = { link = "DiagnosticError" },
+		DiagnosticVirtualTextWarn = { link = "DiagnosticWarn" },
+		DiagnosticVirtualTextInfo = { link = "DiagnosticInfo" },
+		DiagnosticVirtualTextHint = { link = "DiagnosticHint" },
+		DiagnosticVirtualTextOk = { link = "DiagnosticOk" },
+
+		DiagnosticSignError = { link = "DiagnosticError" },
+		DiagnosticSignWarn = { link = "DiagnosticWarn" },
+		DiagnosticSignInfo = { link = "DiagnosticInfo" },
+		DiagnosticSignHint = { link = "DiagnosticHint" },
+		DiagnosticSignOk = { link = "DiagnosticOk" },
+
+		DiagnosticDeprecated = { sp = fg.red, strikethrough = true },
+		DiagnosticUnnecessary = { link = "Comment" },
 	}
 
 	return theme
