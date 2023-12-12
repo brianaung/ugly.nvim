5f1c6fbd585be2eddd43ffc2fb3d279e1e418e75
Brian Aung <brianaung16@gmail.com>
2023-12-12 17:50:36 +1100

    color iteration #2 dark theme focus

--- lua/ugly/colors.lua
+++ lua/ugly/colors.lua
@@ -4,32 +4,31 @@ M.default = {
 	none = "NONE",
 
 	bg = {
-		grey1 = "#0d0c0c",
-		grey2 = "#1D1C19",
-		grey3 = "#282727",
-		grey4 = "#625e5a",
+		grey1 = "#0E0B0D",
+		grey2 = "#161214", -- normal bg
+		grey3 = "#2A2226", -- cursorline
+		grey4 = "#4B3D44", -- visual
 
-		red = "#A34943",
-		yellow = "#73613A",
-		green = "#507150",
-		cyan = "#377677",
-		blue = "#2D596C",
-		-- magenta = "#554270",
-		magenta = "#6A5B6B",
+		-- red = "#A34943",
+		-- yellow = "#73613A",
+		-- green = "#507150",
+		-- cyan = "#377677",
+		-- blue = "#2D596C",
+		magenta = "#4b3d44",
 	},
 
 	fg = {
-		grey1 = "#f0f3f0",
-		grey2 = "#c5c9c5", -- normal fg
-		grey3 = "#a6a69c",
-		grey4 = "#7a8382", -- comment
+		grey1 = "#d2c9a5",
+		grey2 = "#d1b187", -- normal fg
+		grey3 = "#ab9b8e",
+		grey4 = "#847875", -- comment
 
-		red = "#D39892",
-		yellow = "#CCBC99",
-		green = "#97B597",
-		cyan = "#A3D4D5",
-		blue = "#7FB4CA",
-		magenta = "#B8ACB9",
+		red = "#79444a", -- error
+		yellow = "#ba9158", -- warn
+		green = "#b3a555", -- strings -- ok
+		cyan = "#4b726e", -- func -- info
+		blue = "#8caba1", -- identifier -- hint
+		magenta = "#574852",
 	},
 }
 
